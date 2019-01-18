deccea9506b0638ad1a97fe42f44f0c58818f030
Shahriyar Safarov <dba_shahriyar@outlook.com>
2019-01-17 22:06:05 -0800

    XUnitTest Success

---
+++ App2.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using App2;
+using App2.Controllers;
+using Xunit;
+
+namespace App2.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.Equal("Your application description page.", result.ViewBag.Message);
+        }
+
+        [Fact]
+
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
